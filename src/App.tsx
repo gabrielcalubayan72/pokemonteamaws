40fa9531adf6613594007afc8afc65c5d1eb64be
Anton Bolado <jose.bolado@paymaya.com>
2026-01-08 12:51:43 +0800

    feat: fetch names of all pokemon from PokeAPI, sorted alphabetically

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
 import ditto from './assets/ditto.jpeg'
 import './App.css'
 import TeamCreation from './TeamCreation'
 import TeamDisplay from './TeamDisplay'
 
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
 function App() {
   const [count, setCount] = useState(0)
+  const [names, setNames] = useState([]);
+
+  const url = new URL('https://pokeapi.co/api/v2/pokemon/');
+  const params = { limit: "1500", offset: "0" }
+  url.search = new URLSearchParams(params).toString();
+
+  // TODO: Figure out why it still calls the fetchNames function twice.
+  // This is caused by React's strict mode which renders components twice,
+  // but the use of useEffect with an empty dependency array should already
+  // fix this ... so not sure why that's not working.
+  useEffect(() => {
+    const fetchNames = async () => {
+      try {
+        const listOfNames = await 
+          fetch(url)
+            .then((res) => res.json())
+            .then((data) => (data.results.map((pokemon: Pokemon) => pokemon.name)))
+            .catch((err) => console.error("Error:", err));
+        setNames(listOfNames.sort());
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNames();
+  }, []);
+  // console.log(names);
 
   return (
     <>
